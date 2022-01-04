2388bad378afab98fd20c539f635278e57a2d20a
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-04 17:11:40 +0530

    Make sentence_id uneditable for existing Sentences

--- leasikApp/admin.py
+++ leasikApp/admin.py
@@ -3,5 +3,15 @@ from django.contrib import admin
 from .models import Sentence, Word
 
 
-admin.site.register(Sentence)
+class SentenceAdmin(admin.ModelAdmin):
+    """Don't allow a created Sentence to be changed."""
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # we're editing a created object
+            return self.readonly_fields + ('sentence_id',)
+        return self.readonly_fields
+
+admin.site.register(Sentence, SentenceAdmin)
+
+
 admin.site.register(Word)
