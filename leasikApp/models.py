263423529fa47f72fd1fcddd2656407af891cc66
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-04 16:04:47 +0530

    Add a Sentence and a Word model
    These are the two basic models of Leasik. Users try to memorize
    Words by looking at Sentences that have those Words.

--- leasikApp/models.py
+++ leasikApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Sentence(models.Model):
+    sentence_text = models.TextField(primary_key=True)
+
+
+class Word(models.Model):
+    # a provisional list of all languages available
+    language_choices = [
+        ('en', 'English'),
+        ('tr', 'Türkçe'),
+    ]
+
+    word_text = models.CharField(max_length=50)
+    language = models.CharField(choices=language_choices)
+
+    sentences = models.ManyToManyField(Sentence)
+
+    class Meta:
+        unique_together = ('word_text', 'language')
