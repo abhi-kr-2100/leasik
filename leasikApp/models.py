ba122dee10e511bd214bf6170a024acf51b5e4bc
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-04 16:16:04 +0530

    Add __str__ methods to models

--- leasikApp/models.py
+++ leasikApp/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Sentence(models.Model):
     sentence_text = models.TextField(primary_key=True)
 
+    def __str__(self) -> str:
+        return self.sentence_text
+
 
 class Word(models.Model):
     # a provisional list of all languages available
@@ -16,6 +19,9 @@ class Word(models.Model):
     language = models.CharField(max_length=2, choices=language_choices)
 
     sentences = models.ManyToManyField(Sentence)
+
+    def __str__(self) -> str:
+        return self.word_text
 
     class Meta:
         unique_together = ('word_text', 'language')
