e24d80e17bf0cc74395f03079817808b9b64fde0
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-04 19:19:36 +0530

    Add the detail view for a word list

--- leasikApp/views.py
+++ leasikApp/views.py
@@ -1,9 +1,19 @@
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import List
 
 
 class WordListView(ListView):
+    model = List
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return List.objects.filter(owner=self.request.user)
+        return List.objects.none()
+
+
+class ListDetailView(DetailView):
     model = List
 
     def get_queryset(self):
