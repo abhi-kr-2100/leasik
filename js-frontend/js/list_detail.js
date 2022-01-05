8f105656d28cf24c0a872399b7a337c06b68afeb
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-05 22:09:24 +0530

    Add functionality to let user guess whole sentence

--- js-frontend/js/list_detail.js
+++ js-frontend/js/list_detail.js
@@ -2,19 +2,44 @@ const app = Vue.createApp({})
 
 
 app.component('word-test', {
+    data() {
+        return {
+            checked: false,
+            userInput: null
+        }
+    },
+
     props: {
         word: String,
         sentence: String,
         translation: String
     },
+
+    methods: {
+        check() {
+            if (this.checked) {
+                alert("Already checked!")
+                return
+            }
+
+            if (this.userInput === this.sentence) {
+                alert("Correct!")
+            } else {
+                alert("Wrong!")
+            }
+
+            this.checked = true
+        }
+    },
     
     template: `
     <div class="word-test-div">
-        <h2>{{ sentence }}</h2>
+        <h2 v-show="this.checked">{{ sentence }}</h2>
         <p>{{ translation }}</p>
 
         <div class="word-test-buttons">
-            <a>Check</a>
+            <input v-model="userInput">
+            <a @click="check">Check</a>
         </div>
     </div>
     `
