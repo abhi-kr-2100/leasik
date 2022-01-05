bbc3feeb58f7127260f8b42348c61aef0b89f5b5
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-05 15:04:40 +0530

    Pass a proficiency dictionary as context
    This will be used to display the proficiency of a word

--- leasikApp/views.py
+++ leasikApp/views.py
@@ -1,7 +1,7 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-from .models import List
+from .models import List, Proficiency
 
 
 class WordListView(ListView):
@@ -20,3 +20,20 @@ class ListDetailView(DetailView):
         if self.request.user.is_authenticated:
             return List.objects.filter(owner=self.request.user)
         return List.objects.none()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        words = context['object'].words.all()
+        proficiencies = Proficiency.objects.filter(
+            user=self.request.user,
+            word__in=words
+        )
+
+        proficiency_dict = {
+            p.word: p.proficiency for p in proficiencies
+        }
+
+        context['proficiencies'] = proficiency_dict
+
+        return context
