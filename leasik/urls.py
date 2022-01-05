df7e95df4887e485c499ef1a38bb74fb933dcaa1
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-05 10:25:08 +0530

    Namespace URLs

--- leasik/urls.py
+++ leasik/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('leasikApp.urls'))
+    path('', include('leasikApp.urls', namespace='leasikApp'))
 ]
--- leasikApp/urls.py
+++ leasikApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import WordListView, ListDetailView
 
-
+app_name = 'leasikApp'
 urlpatterns = [
     path('', WordListView.as_view(), name='home'),
     path('<slug:slug>', ListDetailView.as_view(), name='list-detail')
