640256b0d99f6ea444b9d8ad7eb7da390b9f2aba
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-06 14:33:19 +0530

    Register login/logout related URLs

--- leasik/urls.py
+++ leasik/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('lists/', include('leasikApp.urls', namespace='leasikApp'))
+    path('lists/', include('leasikApp.urls', namespace='leasikApp')),
+    path('accounts/', include('allauth.urls')),
+    path('logout', LogoutView.as_view())
 ]
