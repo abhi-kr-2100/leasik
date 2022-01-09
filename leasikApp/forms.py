64e6a6b29ee86c63e39167c973bc87b0d332e507
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-09 23:55:15 +0530

    Style the form
    * The text and translations should be TextArea
    * Make the form a flex-box

--- leasikApp/forms.py
+++ leasikApp/forms.py
@@ -9,8 +9,9 @@ class NewWordForm(forms.Form):
 
     word_text = forms.CharField(label='', max_length=50)
     language = forms.ChoiceField(label='', choices=language_choices)
-    text = forms.CharField(label='Sentence text')
-    translation = forms.CharField(label='English translation')
+    text = forms.CharField(label='Sentence text', widget=forms.Textarea)
+    translation = forms.CharField(
+        label='English translation', widget=forms.Textarea)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
