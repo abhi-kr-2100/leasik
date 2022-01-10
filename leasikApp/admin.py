c8bf4870c8dc6bb42d2270d5ad16fc33bda5916a
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-10 17:15:01 +0530

    Remove admin registry of unused models
    These models are no longer used due to the following reasons:
    * Tatoeba sentences were replaced with self-contained ones
    * Word quizzes have been replaced with sentence quizzes

--- leasikApp/admin.py
+++ leasikApp/admin.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
 
 from .models import (
-    Word, List, Proficiency, UserProfile, SelfContainedSentence, SentenceList,
-    SentenceProficiency)
+    UserProfile, SelfContainedSentence, SentenceList, SentenceProficiency)
 
 
 admin.site.register(SelfContainedSentence)
-admin.site.register(Word)
-admin.site.register(List)
-admin.site.register(Proficiency)
 admin.site.register(UserProfile)
 admin.site.register(SentenceList)
 admin.site.register(SentenceProficiency)
