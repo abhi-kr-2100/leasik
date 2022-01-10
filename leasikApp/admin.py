95a78a1f04c7b399b0edc2103f7cf4a8b30a9f09
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-10 16:03:43 +0530

    Register SentenceList to Django admin

--- leasikApp/admin.py
+++ leasikApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Word, List, Proficiency, UserProfile, SelfContainedSentence
+from .models import (
+    Word, List, Proficiency, UserProfile, SelfContainedSentence, SentenceList)
 
 
 admin.site.register(SelfContainedSentence)
@@ -8,3 +9,4 @@ admin.site.register(Word)
 admin.site.register(List)
 admin.site.register(Proficiency)
 admin.site.register(UserProfile)
+admin.site.register(SentenceList)
