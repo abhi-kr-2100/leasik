04362deeef60658b7bbf6c34904106b0aca08b3d
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-10 17:46:52 +0530

    Add views to edit and add Sentences

--- leasikApp/views.py
+++ leasikApp/views.py
@@ -11,10 +11,10 @@ from django.views.generic.detail import DetailView
 from .models import (
     List, Proficiency, Sentence, Word, SelfContainedSentence, SentenceList,
     SentenceProficiency)
-from .forms import NewWordForm
+from .forms import NewWordForm, NewSentenceForm
 from .helpers import (
     get_proficiency_dict, get_sentence_dict, add_word_to_list,
-    get_sentence_proficiency_dict)
+    get_sentence_proficiency_dict, add_sentence_to_list)
 
 
 class SentenceListView(ListView):
@@ -90,6 +90,24 @@ class ListDetailEditView(DetailView):
         return context
 
 
+class SentenceListDetailEditView(DetailView):
+    model = SentenceList
+
+    def get_template_names(self):
+        return ['leasikApp/list_edit.html']
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return SentenceList.objects.filter(owner=self.request.user)
+        return SentenceList.objects.none()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = NewSentenceForm
+
+        return context
+
+
 def add_new_word(request, slug):
     if request.method == 'POST' and request.user.is_authenticated:
         form = NewWordForm(request.POST)
@@ -106,6 +124,23 @@ def add_new_word(request, slug):
         return HttpResponseRedirect(redirect_url)
 
     # TODO: Display form on GET request    
+    return HttpResponseRedirect(reverse('leasikApp:home'))
+
+
+def add_new_sentence(request, slug):
+    if request.method == 'POST' and request.user.is_authenticated:
+        form = NewSentenceForm(request.POST)
+        if form.is_valid():
+            add_sentence_to_list(
+                request.user, slug, form, SelfContainedSentence, SentenceList)
+        else:
+            print("Invalid form")
+            print(request.POST)
+
+        redirect_url = f'{reverse("leasikApp:list-edit", args=[slug])}'
+        return HttpResponseRedirect(redirect_url)
+
+    # TODO: Display form on GET request
     return HttpResponseRedirect(reverse('leasikApp:home'))
 
 
