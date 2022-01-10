a36aa7f7efa66430203655e8b57e0f71cf4bd72c
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-10 16:49:37 +0530

    Add equivalent helpers for SentenceProficiency

--- leasikApp/helpers.py
+++ leasikApp/helpers.py
@@ -132,6 +132,39 @@ def get_or_create_proficiencies(user, words, proficiency_model):
     return proficiencies
 
 
+def get_or_create_sentence_proficiencies(user, sentences, proficiency_model):
+    """Return a list of Proficiency for given words of a given user.
+    
+    If a Proficiency doesn't, create it.
+    """
+
+    proficiencies = []
+    for s in sentences:
+        proficiencies.append(
+            proficiency_model.objects.get_or_create(user=user, sentence=s)[0])
+
+    return proficiencies
+
+
+def get_sentence_proficiency_dict(user, sentences, proficiency_model):
+    """Return an OrderedDict of sentence to proficiency mapping.
+    
+    The dictionary is ordered by proficiency. The considered sentences are
+    passed in through the parameters and are assumed to belong to the given
+    user.
+    """
+
+    proficiencies = sorted(
+        get_or_create_sentence_proficiencies(user, sentences, proficiency_model)
+    )
+
+    proficiency_dict = OrderedDict()
+    for p in proficiencies:
+        proficiency_dict[p.sentence] = p.proficiency
+
+    return proficiency_dict
+
+
 def get_proficiency_dict(user, words, proficiency_model):
     """Return an OrderedDict of word to proficiency mapping.
     
