79baa2ae6a135d67ef21065c9611386bdc53664d
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-12 16:28:16 +0530

    Refactor
    * Reorganize imports
    * Comment on unclear URLs
    * Change URL of add-new-sentence

--- leasikApp/urls.py
+++ leasikApp/urls.py
@@ -1,15 +1,22 @@
 from django.urls import path
 
-from .views import (ListsView, EditListView, PlayListView, update_proficiency, 
-    add_new_sentence)
+from .views import (
+    ListsView, EditListView, PlayListView, update_proficiency,  add_new_sentence
+)
 
 
 app_name = 'leasikApp'
 urlpatterns = [
     path('', ListsView.as_view(), name='home'),
+
+    # post request with an authenticated user and id of sentence will increment
+    # the proficiency by 1
     path('update_proficiency', update_proficiency, name='update-proficiency'),
+
     path('<slug:slug>', PlayListView.as_view(), name='list-detail'),
     path('<slug:slug>/edit', EditListView.as_view(), name='list-edit'),
-    path('<int:pk>/edit/add-new-sentence', add_new_sentence,
-        name='add-new-sentence')
+    
+    # post request with pk of list and the appropriate form in POST will add
+    # a new sentence to the list
+    path('<int:pk>/edit/add-new', add_new_sentence, name='add-new-sentence')
 ]
