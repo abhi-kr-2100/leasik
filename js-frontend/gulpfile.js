fe5866ba728cd4741923ef5ec55b31f75ab40149
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-12 15:11:29 +0530

    Minify .js files when building

--- js-frontend/gulpfile.js
+++ js-frontend/gulpfile.js
@@ -1,11 +1,14 @@
 const gulp = require('gulp')
 const { src, dest } = require('gulp')
+const minify = require('gulp-minify')
+
 const path = require('path')
 
 
 gulp.task('build', function() {
     return (
         src('js/*.js')
+            .pipe(minify())
             .pipe(dest(path.resolve(__dirname, '../static/frontend/js/')))
     )
 })
