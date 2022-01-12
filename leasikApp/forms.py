837822ad43d1eb4471180e655466c09d688b983a
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-12 17:41:40 +0530

    Fix mypy errors
    Some errors still remain but they are due to mypy's limitations.

--- leasikApp/forms.py
+++ leasikApp/forms.py
@@ -5,7 +5,7 @@ class NewSentenceForm(forms.Form):
     text = forms.CharField(label='Text', widget=forms.Textarea)
     translation = forms.CharField(label='Translation', widget=forms.Textarea)
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self.fields['text'].widget.attrs.update({'autofocus': 'autofocus'})
--- leasikApp/helpers.py
+++ leasikApp/helpers.py
@@ -1,10 +1,16 @@
 """Helper or utility functions for the leasikApp app."""
 
 
+from typing import List
+
+from django.db.models.query import QuerySet
+from django.contrib.auth.models import User
+
+from .forms import NewSentenceForm
 from .models import Proficiency, Sentence
 
 
-def get_sentence_from_form(form):
+def get_sentence_from_form(form: NewSentenceForm) -> Sentence:
     """Create and return a sentence object from the given form.
     
     If the sentence already exists, just return it.
@@ -16,19 +22,20 @@ def get_sentence_from_form(form):
     )[0]
 
 
-def update_proficiency_helper(user, sentence_id):
+def update_proficiency_helper(user: User, sentence_id: int):
     """Update the proficiency between the given user and sentence."""
 
-    the_sentence = Sentence.objects.get(id=sentence_id)
-    the_proficiency = Proficiency.objects.get_or_create(
+    the_sentence: Sentence = Sentence.objects.get(id=sentence_id)
+    the_proficiency: Proficiency = Proficiency.objects.get_or_create(
         user=user, sentence=the_sentence)[0]
 
-    new_proficiency = (the_proficiency.proficiency + 1) % 100
+    new_proficiency: int = (the_proficiency.proficiency + 1) % 100
     the_proficiency.proficiency = new_proficiency
     the_proficiency.save(update_fields=['proficiency'])
 
 
-def get_sentences_in_order(user, sentences):
+def get_sentences_in_order(user: User, sentences: QuerySet[Sentence]) -> \
+        List[Sentence]:
     """Return a copy of sentences in the correct order.
     
     The order is determined as follows:
