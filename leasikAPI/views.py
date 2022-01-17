6cca9ec0b1aa33809cc1183a49d3b71e8250ac75
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-17 16:35:37 +0530

    Create a ViewSet for the SentenceList model

--- leasikAPI/views.py
+++ leasikAPI/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from typing import List
 
-# Create your views here.
+from django.shortcuts import get_object_or_404
+
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+
+from leasikApp.models import SentenceList
+from .serializers import SentenceListSerializer
+from .permissions import IsOwnerOrReadonlyPublic
+
+
+class SentenceListViewSet(ModelViewSet):
+    serializer_class = SentenceListSerializer
+    permission_classes = (IsAuthenticated, IsOwnerOrReadonlyPublic)
+
+    def get_queryset(self) -> List[SentenceList]:
+        qs = set().union(SentenceList.objects.filter(is_public=True))
+        qs = qs.union(SentenceList.objects.filter(owner=self.request.user))
+
+        return list(qs)
+
+    def get_object(self):
+        obj = get_object_or_404(SentenceList, slug=self.kwargs['pk'])
+        self.check_object_permissions(self.request, obj)
+
+        return obj
