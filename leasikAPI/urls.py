e976b0a8d6349d05202a1e97045be18403cb7510
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-17 19:42:30 +0530

    Add API paths for SentenceNote

--- leasikAPI/urls.py
+++ leasikAPI/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import SentenceListViewSet, SentenceViewSet
+from .views import SentenceListViewSet, SentenceViewSet, SentenceNoteViewSet
 
 
 router = DefaultRouter()
 router.register('lists', SentenceListViewSet, basename='SentenceList')
 router.register('sentences', SentenceViewSet, basename='Sentence')
+router.register('notes', SentenceNoteViewSet, basename='SentenceNote')
 
 
 app_name = 'leasikAPI'
--- leasikAPI/views.py
+++ leasikAPI/views.py
@@ -5,10 +5,14 @@ from django.shortcuts import get_object_or_404
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
-from leasikApp.models import Sentence, SentenceList
+from leasikApp.models import Sentence, SentenceList, SentenceNote
 from leasikApp.helpers import get_unique_slug
-from .serializers import SentenceListSerializer, SentenceSerializer
-from .permissions import IsOwnerOrReadonlyPublic, IsOnlyAddingOrReadonly
+from .serializers import (
+    SentenceListSerializer, SentenceNoteSerializer, SentenceSerializer
+)
+from .permissions import (
+    IsOwnerOrReadonlyPublic, IsOnlyAddingOrReadonly, IsOwnerOrReject
+)
 
 
 class SentenceListViewSet(ModelViewSet):
@@ -38,3 +42,20 @@ class SentenceViewSet(ModelViewSet):
     serializer_class = SentenceSerializer
     queryset = Sentence.objects.all()
     permission_classes = (IsAuthenticated, IsOnlyAddingOrReadonly)
+
+
+class SentenceNoteViewSet(ModelViewSet):
+    serializer_class = SentenceNoteSerializer
+    permission_classes = (IsAuthenticated, IsOwnerOrReject)
+
+    def get_queryset(self):
+        return SentenceNote.objects.filter(owner=self.request.user)
+
+    def get_object(self):
+        obj = get_object_or_404(SentenceNote, pk=self.kwargs['pk'])
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
