9f441e4de4d6ea6633fe9cd3d46fe54972cedc23
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-01-24 13:52:57 +0530

    Replace `Proficiency` and `Note` with `Card`
    The `Card` model represents a user-specific flashcard for each
    `Sentence`. It contains details to determine (using SM-2 algorithm)
    how easy the card is for the user. Additionally, it contains any notes
    the user might have added to it.
    
    This commit:
    * adds the `Card` model
    * removes `Proficiency` and `SentenceNote` models
    * registers the `Card` model to Django admin

--- leasikApp/admin.py
+++ leasikApp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import SentenceList, Sentence
+from .models import SentenceList, Sentence, Card
 
 
 admin.site.register(SentenceList)
 admin.site.register(Sentence)
+admin.site.register(Card)
