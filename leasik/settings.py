84f3750a28f4899c32b0d6935aab08d0dad0626e
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-02-06 12:19:40 +0530

    Set up basic authentication

--- leasik/settings.py
+++ leasik/settings.py
@@ -162,5 +162,11 @@ SESSION_COOKIE_SECURE = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication'
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+    ]
 }
