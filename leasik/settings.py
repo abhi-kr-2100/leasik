a3cdca377159a353fd28de841a0d1f6ee8f796dc
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-02-06 13:07:46 +0530

    Disallow unauthenticated users from making changes

--- leasik/settings.py
+++ leasik/settings.py
@@ -167,6 +167,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     ]
 }
