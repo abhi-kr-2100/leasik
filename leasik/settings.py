2791bc1736845af9adeef4a2ca7c728f69ecdea8
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-02-13 20:58:16 +0530

    Install django-cors-headers
    Also, allow localhost:3000 to make requests.

--- leasik/settings.py
+++ leasik/settings.py
@@ -53,9 +53,11 @@ INSTALLED_APPS = [
     "allauth.socialaccount",
     "allauth.socialaccount.providers.google",
     "rest_framework",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -172,3 +174,8 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
     ],
 }
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
