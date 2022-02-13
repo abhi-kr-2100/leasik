aa35eab950917b0980f3d6616771d82118968256
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-02-13 14:21:46 +0530

    Start a new react app
    The app is started using `create-react-app` with the
    `typescript` template.
    
    Additionally, customize the default app to make it
    minimal.

---
+++ leasik-react/src/App.tsx
@@ -0,0 +1,12 @@
+import { Component, ReactNode } from 'react'
+
+
+export default class App extends Component {
+    render(): ReactNode {
+        return (
+            <div>
+                <h1>Leasik</h1>
+            </div>
+        )
+    }
+}
