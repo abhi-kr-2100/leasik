3d10bad07f190e01803a82ef9d1b078f6d9645cd
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-02-14 23:58:03 +0530

    Remove unused code

--- leasik-react/src/App.tsx
+++ leasik-react/src/App.tsx
@@ -1,8 +1,6 @@
 import { Component, ReactNode } from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
 
-import Home from './routes/Home'
-
 
 export default class App extends Component {
     render(): ReactNode {
