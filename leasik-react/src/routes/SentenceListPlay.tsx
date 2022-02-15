7a99fe04a0d41af86e77d6119a92f6cb2703c37b
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-02-15 14:43:01 +0530

    Render the first sentence

--- leasik-react/src/routes/SentenceListPlay.tsx
+++ leasik-react/src/routes/SentenceListPlay.tsx
@@ -5,14 +5,32 @@ import { getToken } from '../authentication/utils'
 import axios from 'axios'
 
 
+export type Card = {
+    id: number
+    repetition_number: number
+    easiness_facotr: number
+    inter_repetition_interval: string
+    last_review_date: string
+    note: string
+    owner: {
+        id: number
+    }
+    sentence: {
+        id: number
+        text: string
+        translation: string
+        text_language: string
+        translation_language: string
+    }
+    hidden_word_position: number
+}
+
 export type SentenceListPlayProps = {
     sentenceListId: number
 }
 
 export type SentenceListPlayState = {
-    cards: Array<{
-        id: number
-    }>
+    cards: Array<Card>
     currentCardIndex: number
 
     token: string | null
@@ -68,7 +86,39 @@ export class SentenceListPlay extends Component<SentenceListPlayProps, SentenceL
     }
 
     renderCardScreen(cardIndex: number): ReactNode {
-        return <div>Rendering { cardIndex }...</div>
+        const cardToRender: Card = this.state.cards[cardIndex]
+        return (
+            <div>
+                <div className='hero-head'>
+                    <div className='block'></div>
+                </div>
+
+                <div className='hero-body'>
+                    <div className='container has-text-centered'>
+                        <div className='block'>
+                            <p className='title is-3'>{ cardToRender.sentence.text }</p>
+                        </div>
+
+                        <div className='block'>
+                            <p className='title is-6'>{ cardToRender.sentence.translation }</p>
+                            <div className='block'>
+                                <textarea className='p-2'>
+                                    { cardToRender.note }
+                                </textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='hero-footer'>
+                    <div className='container has-text-centered block'>
+                        <button className='button is-primary'>Check</button>
+                    </div>
+
+                    <div className='block'></div>
+                </div>
+            </div>
+        )
     }
 }
 
