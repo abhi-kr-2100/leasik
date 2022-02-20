02ee73ec492a52190f007ecde7b7757730b31a80
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-02-20 18:23:26 +0530

    Configure linting by ESLint

--- leasik-react/.eslintrc.js
+++ leasik-react/.eslintrc.js
@@ -19,5 +19,14 @@ module.exports = {
         sourceType: "module",
     },
     plugins: ["react", "@typescript-eslint", "promise", "unicorn"],
-    rules: {},
+    rules: {
+        "react/react-in-jsx-scope": 0,
+        "unicorn/prefer-query-selector": 1,
+        "unicorn/filename-case": 1,
+        "unicorn/no-null": 1,
+        "unicorn/no-array-callback-reference": 0,
+        "@typescript-eslint/no-empty-function": 1,
+        "promise/always-return": 1,
+        "promise/catch-or-return": 1
+    },
 };
