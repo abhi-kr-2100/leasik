da4792ba397b93bf019c8f8c4f08877456d13401
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-02-21 13:55:02 +0530

    Add leasikREST to INSTALLED_APPS

--- leasik/settings.py
+++ leasik/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # "django.contrib.sites",
     "leasikApp",
+    "leasikREST",
     "rest_framework",
     "rest_framework.authtoken",
     "corsheaders",
