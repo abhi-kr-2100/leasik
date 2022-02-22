6735580b48295a66cbff0d4173e0c7955c6eecd1
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-02-22 13:39:46 +0530

    Configure ESLint

--- leasik-react/.eslintrc.js
+++ leasik-react/.eslintrc.js
@@ -21,12 +21,15 @@ module.exports = {
     plugins: ["react", "@typescript-eslint", "promise", "unicorn"],
     rules: {
         "react/react-in-jsx-scope": 0,
-        "unicorn/prefer-query-selector": 1,
-        "unicorn/filename-case": 1,
+        "unicorn/prefer-query-selector": 0,
+        "unicorn/filename-case": 0,
         "unicorn/no-null": 1,
         "unicorn/no-array-callback-reference": 0,
         "@typescript-eslint/no-empty-function": 1,
         "promise/always-return": 1,
-        "promise/catch-or-return": 1
+        "promise/catch-or-return": [
+            1,
+            {"allowFinally": true}
+        ]
     },
 };
