7270f4cdbdb6ae7859dc546f2ddfa6e2ce044624
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-03-07 17:27:14 +0530

    Add AugmentedCardSerializer
    This serializer contains the "is_bookmarked" field with the
    usual Card fields.

--- leasikREST/serializers.py
+++ leasikREST/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer, CharField
+from rest_framework.serializers import ModelSerializer, CharField, BooleanField
 
 from leasikApp.models import Card, Sentence, Bookmark, SentenceList
 
@@ -18,6 +18,23 @@ class SentenceSerializer(ModelSerializer):
 class CardSerializer(ModelSerializer):
     id = CharField()  # id may be too big for JavaScript's number type
     sentence = SentenceSerializer()
+
+    class Meta:
+        model = Card
+        fields = [
+            "id",
+            "note",
+            "sentence",
+            "hidden_word_position",
+        ]
+
+
+class AugmentedCardSerializer(ModelSerializer):
+    """Serializer for the Card object, with the extra field is_bookmarked."""
+
+    id = CharField()  # id may be too big for JavaScript's number type
+    sentence = SentenceSerializer()
+    is_bookmarked = BooleanField()
 
     class Meta:
         model = Card
