ce947084b6d7c230042d88a42595435d69da59fa
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-06-05 18:51:25 +0530

    Replace Home component with HomeHero
    HomeHero is also renamed Home.

--- leasik-frontend/src/components/Home.tsx
+++ leasik-frontend/src/components/Home.tsx
@@ -1,11 +1,23 @@
-import NavBar from "./NavBar";
-import HomeHero from "./HomeHero";
-
 export default function Home() {
   return (
-    <div className="h-screen">
-      <NavBar />
-      <HomeHero />
+    <div className="flex flex-col h-[90%] bg-emerald-400 pl-[10%] pt-[17%] text-white">
+      <main className="mb-10">
+        <h1 className="text-8xl mb-1">Leasik</h1>
+        <p className="pl-2 text-2xl">Learn languages in context</p>
+      </main>
+      <footer>
+        <p className="pl-2 text-xl">
+          Created by{" "}
+          <a
+            href="https://github.com/abhi-kr-2100"
+            target="_blank"
+            rel="noreferrer"
+            className="underline font-black hover:italic"
+          >
+            abhi-kr-2100
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
--- leasik-frontend/src/index.tsx
+++ leasik-frontend/src/index.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
-import HomeHero from "./components/HomeHero";
+import Home from "./components/Home";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -15,7 +15,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route index element={<HomeHero />} />
+          <Route index element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
