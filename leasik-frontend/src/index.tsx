6af91e7bf8436e47d48aecd7a698cdd08bdd359c
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-06-05 12:17:02 +0530

    Replace App with a Home component

--- leasik-frontend/src/index.tsx
+++ leasik-frontend/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import App from "./App";
+import Home from "./components/Home";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,7 +12,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
