22ebe2431cfd8e0d9e90431c702085475a90aaa0
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-06-05 14:11:38 +0530

    Implement a dummy navbar component

---
+++ leasik-frontend/src/components/NavBar.tsx
@@ -0,0 +1,33 @@
+export default function NavBar() {
+  return (
+    <nav className="px-5 bg-emerald-400 py-3">
+      <ul className="flex gap-x-1">
+        <li className="text-white hover:bg-emerald-600 rounded">
+          <a href="/" className="block py-2 px-2 mx-1">
+            Home
+          </a>
+        </li>
+        <li className="text-white hover:bg-emerald-600 rounded">
+          <a href="/lists" className="block py-2 px-2 mx-1">
+            Lists
+          </a>
+        </li>
+        <li className="text-white hover:bg-emerald-600 rounded">
+          <a
+            href="https://github.com/abhi-kr-2100/"
+            target="_blank"
+            rel="noreferrer"
+            className="block py-2 px-2 mx-1"
+          >
+            abhi-kr-2100
+          </a>
+        </li>
+        <li className="text-white hover:bg-emerald-600 rounded ml-auto">
+          <a href="/login" className="block py-2 px-2 mx-1">
+            Login
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
