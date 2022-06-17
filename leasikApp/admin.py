f53c9447a24f9aeaf997652f56ab37f02486e6de
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-06-17 21:33:32 +0530

    Register Sentence and Card to Django admin

--- leasikApp/admin.py
+++ leasikApp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import SentenceList
+from .models import SentenceList, Sentence, Card
 
 
 admin.site.register(SentenceList)
+admin.site.register(Sentence)
+admin.site.register(Card)
