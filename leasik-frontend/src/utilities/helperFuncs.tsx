f18a9b542be6a3ec5db5908b26a6c7ceb06c862f
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-06-18 14:34:10 +0530

    Deal with HWPs of -1 better
    * -1s should only be replaced with a random value if -1 is the
    main hidden word position.
    
    See added comment in the code for elaboration.

--- leasik-frontend/src/utilities/helperFuncs.tsx
+++ leasik-frontend/src/utilities/helperFuncs.tsx
@@ -6,14 +6,24 @@ export function normalizedCard(card: Card): Card {
 
   const newCard: Card = {
     ...card,
-    hiddenWordPosition:
-      card.hiddenWordPosition === -1
-        ? randomReplacement
-        : card.hiddenWordPosition,
-    hiddenWordPositions: card.hiddenWordPositions.map((pos) =>
-      pos === -1 ? randomReplacement : pos
-    ),
   };
+
+  // The job of this function is to deal with a hidden word position of -1
+  // consistently, i.e., all -1s should map to a single value. However, only
+  // the hidden word position of the card is significant (as opposed to the
+  // hidden word positions of the sister cards). Hence, when the hidden word
+  // position of the main card is already valid, we simply remove all -1s from
+  // the hidden word positions of the sister cards.
+  if (newCard.hiddenWordPosition === -1) {
+    newCard.hiddenWordPosition = randomReplacement;
+    newCard.hiddenWordPositions = card.hiddenWordPositions.map((hwp) =>
+      hwp === -1 ? randomReplacement : hwp
+    );
+  } else {
+    newCard.hiddenWordPositions = newCard.hiddenWordPositions.filter(
+      (hwp) => hwp !== -1
+    );
+  }
 
   return newCard;
 }
