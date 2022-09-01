56202494cd9a40bf00eeda01fed700f38c9c2745
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-09-01 17:10:53 +0530

    Replace CardModelTests with WordCardModelTests

--- leasikApp/tests.py
+++ leasikApp/tests.py
@@ -3,28 +3,35 @@ from datetime import timedelta, date
 from django.test import TestCase
 from django.contrib.auth.models import User
 
-from .models import Card, Sentence
+from .models import SentenceList, WordCard
 
 
-class CardModelTests(TestCase):
+class WordCardModelTests(TestCase):
+    def setUp(self):
+        User.objects.create(username="mock", password="mock")
+        SentenceList.objects.create(
+            name="mock sentence list", owner=User.objects.get()
+        )
+
     def test_is_up_for_review_with_newly_created_card(self):
         """A newly created card is up for review."""
 
-        mock_owner = User()
-        mock_sentence = Sentence(text="foo", translation="bar")
-
-        newly_created_card = Card(owner=mock_owner, sentence=mock_sentence)
-        newly_created_card.last_review_date = date.today()
+        newly_created_card = WordCard.objects.create(
+            sentence_list=SentenceList.objects.get(),
+            owner=User.objects.get(),
+            word="mock",
+        )
 
         self.assertTrue(newly_created_card.is_up_for_review())
 
     def test_is_up_for_review_with_old_card(self):
         """An old card is up for review."""
 
-        mock_owner = User()
-        mock_sentence = Sentence(text="foo", translation="bar")
-
-        old_card = Card(owner=mock_owner, sentence=mock_sentence)
+        old_card = WordCard.objects.create(
+            sentence_list=SentenceList.objects.get(),
+            owner=User.objects.get(),
+            word="mock",
+        )
         old_card.last_review_date = date.today() - timedelta(days=10)
         old_card.inter_repetition_interval = timedelta(days=1)
 
@@ -33,10 +40,11 @@ class CardModelTests(TestCase):
     def test_is_up_for_review_with_large_inter_repetition_interval(self):
         """A card with a large inter repetition interval is not up for review."""
 
-        mock_owner = User()
-        mock_sentence = Sentence(text="foo", translation="bar")
-
-        card = Card(owner=mock_owner, sentence=mock_sentence)
+        card = WordCard.objects.create(
+            sentence_list=SentenceList.objects.get(),
+            owner=User.objects.get(),
+            word="mock",
+        )
         card.last_review_date = date.today() - timedelta(days=10)
         card.inter_repetition_interval = timedelta(days=100)
 
@@ -45,11 +53,11 @@ class CardModelTests(TestCase):
     def test_is_up_for_review_for_recently_reviewed_card(self):
         """A recently reviewed card is not up for review."""
 
-        mock_owner = User()
-        mock_sentence = Sentence(text="foo", translation="bar")
-
-        card = Card(owner=mock_owner, sentence=mock_sentence)
-        card.last_review_date = date.today()
+        card = WordCard.objects.create(
+            sentence_list=SentenceList.objects.get(),
+            owner=User.objects.get(),
+            word="mock",
+        )
         card.inter_repetition_interval = timedelta(days=1)
 
         self.assertFalse(card.is_up_for_review())
