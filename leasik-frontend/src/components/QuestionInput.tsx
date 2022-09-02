29a3720f2c72acb45a620c09e0e4b7a1b25faf3b
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-09-02 17:10:20 +0530

    Render question from a given WordCard

--- leasik-frontend/src/components/QuestionInput.tsx
+++ leasik-frontend/src/components/QuestionInput.tsx
@@ -1,9 +1,9 @@
-import Card from "../models/Card";
-import { toWords } from "../utilities/helperFuncs";
+import WordCard from "../models/WordCard";
+import { findWordPositions, randomChoice, toWords } from "../utilities/helperFuncs";
 import { InputStatusType } from "../utilities/types";
 
 export interface IQuestionInputProps {
-  card: Card;
+  wordCard: WordCard;
 
   userInput: string;
   setUserInput: (input: string) => void;
@@ -13,9 +13,34 @@ export interface IQuestionInputProps {
 }
 
 export default function QuestionInput(props: IQuestionInputProps) {
+  const words = toWords(props.wordCard.sentence.text);
+  const hiddenWordPosition = randomChoice(findWordPositions(words, props.wordCard.word));
+  const beforeHiddenWord = words.slice(0, hiddenWordPosition).join(" ");
+  const hiddenWord = words[hiddenWordPosition];
+  const afterHiddenWord = words.slice(hiddenWordPosition + 1).join(" ");
+
+  const inputBgColorClass =
+    props.inputStatus === "correct"
+      ? "bg-green-300"
+      : props.inputStatus === "incorrect"
+        ? "bg-red-300"
+        : "";
+
   return (
     <div className="text-xl my-3">
-      <p>props.card.sentence.text</p>
+      <p className="inline">{beforeHiddenWord}</p>
+      <input
+        className={`inline mx-1 p-2 text-center ${inputBgColorClass}`}
+        autoFocus
+        value={props.inputStatus !== "unchecked" ? hiddenWord : props.userInput}
+        onChange={(e) => props.setUserInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            props.primaryAction();
+          }
+        }}
+      />
+      <p className="inline">{afterHiddenWord}</p>
     </div>
   );
 }
--- leasik-frontend/src/utilities/helperFuncs.tsx
+++ leasik-frontend/src/utilities/helperFuncs.tsx
@@ -2,6 +2,21 @@ export function toWords(text: string): string[] {
   return text.split(/\s+/);
 }
 
+export function findWordPositions(words: string[], word: string): number[] {
+  let positions: number[] = [];
+  for (let i = 0; i < words.length; ++i) {
+    if (matches(word, words[i])) {
+      positions.push(i);
+    }
+  }
+
+  return positions;
+}
+
+export function randomChoice<T>(items: T[]): T {
+  return items[randRange(items.length)];
+}
+
 export function randRange(max: number): number {
   return Math.floor(Math.random() * max);
 }
