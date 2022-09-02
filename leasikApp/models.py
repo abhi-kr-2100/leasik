e6902dc432044cb291bea9e8cd1667346f241250
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-09-02 15:14:23 +0530

    Fix bug in create_user_profile
    The field name is owner not user.

--- leasikApp/models.py
+++ leasikApp/models.py
@@ -146,4 +146,4 @@ class UserProfile(models.Model):
     @receiver(post_save, sender=settings.AUTH_USER_MODEL)
     def create_user_profile(sender, instance, created, **kwargs):
         if created:
-            UserProfile.objects.create(user=instance)
+            UserProfile.objects.create(owner=instance)
