97b847da4c6f1beb737569aba24b7871ebe4af32
Abhishek Kumar <abhi.kr.2100@gmail.com>
2023-07-09 09:58:45 +0530

    Register Tag and UserProfile to Django admin
    Tag and UserProfile should be editable via Django admin
    interface.
    
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

--- leasikApp/admin.py
+++ leasikApp/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import SentenceList, Sentence, WordCard
+from .models import SentenceList, Sentence, WordCard, Tag, UserProfile
 
 
 admin.site.register(SentenceList)
 admin.site.register(Sentence)
 admin.site.register(WordCard)
+admin.site.register(Tag)
+admin.site.register(UserProfile)
