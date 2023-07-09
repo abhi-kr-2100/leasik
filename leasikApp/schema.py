bc412ed40a7c00048d7f477831297ec0048ab041
Abhishek Kumar <abhi.kr.2100@gmail.com>
2023-07-09 12:19:56 +0530

    Refactor resolve_word_cards
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

--- leasikApp/schema.py
+++ leasikApp/schema.py
@@ -4,6 +4,7 @@ from datetime import date
 from string import digits, punctuation, whitespace
 
 from django.db.models import Q, F
+from django.conf import settings
 
 import graphene
 from graphene import relay
@@ -74,30 +75,53 @@ class Query(graphene.ObjectType):
         return SentenceList.objects.filter(
             Q(is_public=True) | Q(owner=info.context.user)
         )
+    
+    def _setup_for_first_play(
+            user: settings.AUTH_USER_MODEL,
+            sentence_list: SentenceList
+    ):
+        """Set things up if user is playing a list for the first time."""
 
-    def resolve_word_cards(root, info, sentence_list_id, **kwargs):
-        if info.context.user.is_anonymous:
-            raise Exception("Not authenticated")
+        user_profile = UserProfile.objects.get(owner=user)
+        if user_profile.played_lists.contains(sentence_list):
+            return
+        sentence_list.prepare_word_cards(owner=True, in_bulk=True)
 
-        user_profile = UserProfile.objects.get(owner=info.context.user)
-        sl_int_id = int(from_global_id(sentence_list_id)[1])
-        sl = SentenceList.objects.get(id=sl_int_id)
-        if not user_profile.played_lists.contains(sl):
-            sl.prepare_word_cards(info.context.user, in_bulk=True)
-            user_profile.played_lists.add(sl)
+    def _get_sentence_list_from_gql_id(id) -> int:
+        """Return the SentenceList given GraphQL's global ID."""
 
-        # higher score means needs reviewing more
-        with_reviewable_score = WordCard.objects.annotate(
-            reviewable_score=(
+        normal_id = int(from_global_id(id)[1])
+        return SentenceList.objects.get(id=normal_id)
+
+    def _get_word_cards_ordered_by_reviewableness(
+            owner: settings.AUTH_USER_MODEL,
+            sentence_list: SentenceList
+    ) -> Iterable[WordCard]:
+        """Return an ordered list of word cards for the given list and owner.
+        
+        The word cards are ordered based on the need to review them.
+        """
+
+        with_reviewableness_score = WordCard.objects.annotate(
+            reviewableness_score=(
                 date.today()
                 - F("last_review_date")
                 - F("inter_repetition_interval")
             )
         )
 
-        return with_reviewable_score.filter(
-            sentence_list=sl, owner=info.context.user
-        ).order_by("-reviewable_score", "?")
+        return with_reviewableness_score.filter(
+            sentence_list=sentence_list, owner=owner
+        ).order_by("-reviewableness_score")
+
+    def resolve_word_cards(root, info, sentence_list_id, **kwargs):
+        if info.context.user.is_anonymous:
+            raise Exception("Not authenticated")
+        
+        sentence_list = Query._get_sentence_list_from_gql_id(sentence_list_id)
+        Query._setup_for_first_play(info.context.user, sentence_list)
+        return Query._get_word_cards_ordered_by_reviewableness(
+            info.context.user, sentence_list)
 
 
 class UpdateProficiency(relay.ClientIDMutation):
