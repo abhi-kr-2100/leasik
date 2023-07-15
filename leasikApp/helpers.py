200afc7a81f697eca2173cb3d5eec3f3fa8ded51
Abhishek Kumar <abhi.kr.2100@gmail.com>
2023-07-15 12:20:17 +0530

    Clarify comments and variable names
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

--- leasikApp/helpers.py
+++ leasikApp/helpers.py
@@ -12,11 +12,10 @@ def weighted_avg(weights_with_terms: Iterable[Tuple[float, float]]) -> float:
 def get_overall_proficiency_score(word_scores: Iterable):
     """Return the weighted average proficiency score of the given WordScores.
 
-    The average is weighted by the inverse of the easiness factor (difficulty
-    factor). The higher the difficulty factor, the more it contributes to the
-    average.
+    The average is weighted by the inverse of the easiness factor (whcih could
+    be called the difficulty factor). The higher the difficulty factor, the
+    more it contributes to the average.
     """
-
     weights_with_terms = [
         (1 / ws.easiness_factor, ws.get_proficiency_score())
         for ws in word_scores
@@ -26,35 +25,44 @@ def get_overall_proficiency_score(word_scores: Iterable):
 
 
 def sm2(
-    q: int, n: int, ef: float, i: timedelta
+    score: int,
+    repetition_number: int,
+    easiness_factor: float,
+    inter_repetition_interval: timedelta,
 ) -> Tuple[int, float, timedelta]:
     """Implement the SM-2 SRS algorithm.
 
     See https://en.wikipedia.org/wiki/SuperMemo#Description_of_SM-2_algorithm.
     """
-    if q >= 3:
-        if n == 0:
-            i = timedelta(days=1)
-        elif n == 1:
-            i = timedelta(days=6)
+    if score >= 3:
+        if repetition_number == 0:
+            inter_repetition_interval = timedelta(days=1)
+        elif repetition_number == 1:
+            inter_repetition_interval = timedelta(days=6)
         else:
-            i = timedelta(days=round(i.days * ef))
-        n += 1
+            inter_repetition_interval = timedelta(
+                days=round(inter_repetition_interval.days * easiness_factor)
+            )
+        repetition_number += 1
     else:
-        n = 0
-        i = timedelta(days=1)
+        repetition_number = 0
+        inter_repetition_interval = timedelta(days=1)
 
-    ef = ef + (0.1 - (5 - q) * (0.08 + (5 - q) * 0.02))
-    ef = max(ef, 1.3)
+    easiness_factor = easiness_factor + (
+        0.1 - (5 - score) * (0.08 + (5 - score) * 0.02)
+    )
+    easiness_factor = max(easiness_factor, 1.3)
 
     # As values of n greater than or equal to 2 are treated the
     # same, we cap n at 2. This prevents overflow in case of very
     # large values of n.
-    n = n % 3
+    repetition_number = repetition_number % 3
 
     # SM-2 doesn't specify any upper limit on inter-repetition interval.
     # However, since computer memory is finite, we're capping it at a very
     # large time interval.
-    i = min(i, timedelta(days=365 * 10))
+    inter_repetition_interval = min(
+        inter_repetition_interval, timedelta(days=365 * 10)
+    )
 
-    return (n, ef, i)
+    return (repetition_number, easiness_factor, inter_repetition_interval)
