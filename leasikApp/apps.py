9b28816ddb2e12a4882dae8ee8ba65d3e4c85313
Abhishek Kumar <abhi.kr.2100@gmail.com>
2023-07-15 12:15:06 +0530

    Clarify the use of BigAutoField as the default auto field
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

--- leasikApp/apps.py
+++ leasikApp/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class LeasikappConfig(AppConfig):
+    # Some DBs — such as CockroachDB — use very large default IDs. We need
+    # BigAutoField for those.
     default_auto_field = "django.db.models.BigAutoField"
+
     name = "leasikApp"
