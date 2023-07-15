158a84b17aaf268942dc2419d379d13eab59792b
Abhishek Kumar <abhi.kr.2100@gmail.com>
2023-07-15 13:38:22 +0530

    Choose masked word based on proficiency
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

--- leasik-frontend/src/utilities/helperFuncs.tsx
+++ leasik-frontend/src/utilities/helperFuncs.tsx
@@ -1,4 +1,4 @@
-import { trim } from "lodash";
+import { min, trim } from "lodash";
 import { Sentence, Word } from "./types";
 
 const punctuation = `!"#$%&'()*+,-./:;<=>?@[\\]^_\`{|}~`;
@@ -9,16 +9,15 @@ export function toWords(text: string): string[] {
   return text.split(/\s+/);
 }
 
+/**
+ * Choose the word to hide based on the proficiency of each word in the
+ * sentence.
+ */
 export function chooseMaskedWord(sentence: Sentence): Word {
-  return randomChoice(sentence.words);
-}
+  const prob_nums = getProbabilityNumbers(sentence.words.map(word => word.score));
+  const prob_array = getSpaceByProbabilityNumbers(sentence.words, prob_nums);
 
-export function randomChoice<T>(items: T[]): T {
-  return items[randRange(items.length)];
-}
-
-export function randRange(max: number): number {
-  return Math.floor(Math.random() * max);
+  return randomChoice(prob_array);
 }
 
 export function matches(s1: string, s2: string, locale: string = "") {
@@ -38,4 +37,43 @@ export function matches(s1: string, s2: string, locale: string = "") {
 export function startsWith(prefix: string, str: string, locale: string = "") {
   const barePrefix = trim(prefix, punctuation + whitespace + digits);
   return matches(barePrefix, str.slice(0, barePrefix.length), locale);
+}
+
+/**
+ * Probability number is the number of times an element should occur in a
+ * collection so that a random uniform choice from that collection would bias
+ * the overall choice according to the weights.
+ * 
+ * For example, in a weighted coin toss if H is twice as likely as T, instead
+ * of dealing with probability distributions, we can suppose the space to
+ * [H, H, T] instead of [H, T]. Here, the probability number of H is 2, and the
+ * probability number of T is 1.
+ */
+function getProbabilityNumbers(weights: number[]) {
+  const normalized = normalizedWeights(weights);
+  return normalized;
+}
+
+function getSpaceByProbabilityNumbers<T>(items: T[], probNums: number[]) {
+  let space: T[] = [];
+  items.forEach((item, idx) => {
+    const repeated = Array(probNums[idx]).fill(item);
+    space.push(...repeated);
+  });
+
+  return space;
+}
+
+/** Make all weights positive (greater than 0). */
+function normalizedWeights(weights: number[]) {
+  const base = Math.abs(min(weights) ?? 0) + 1; // plus one to make sure everything is > 0
+  return weights.map(w => w + base);
+}
+
+export function randomChoice<T>(items: T[]): T {
+  return items[randRange(items.length)];
+}
+
+export function randRange(max: number): number {
+  return Math.floor(Math.random() * max);
 }
